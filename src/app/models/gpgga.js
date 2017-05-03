374c83242bab9225db0c238bf45a7f7f8c3dbbe1
Watchanon Numnam <jiramot@gmail.com>
2017-05-03 14:57:11 +0700

    Add model gpgga and gprmc

---
+++ src/app/models/gpgga.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const GpggaSchema = new Schema({
+  deviceId: { type: String, required: true },
+  date: { type: Date, required: true },
+  coord: {
+    lng: { type: Number, required: true },
+    lat: { type: Number, required: true }
+  },
+  positionFixIndicator: { type: Number },
+  satellitesUsed: { type: Number },
+  hdop: { type: Number },
+  mslAltitude: { type: Number },
+  geoidSeparation: { type: Number },
+  checksum: { type: String }
+}, {
+  timestamps: true,
+  collection: 'gpgga' })
+
+export default mongoose.model('Gpgga', GpggaSchema)
---
+++ src/app/models/gprmc.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const GprmcSchema = new Schema({
+  deviceId: { type: String, required: true },
+  date: { type: Date, required: true },
+  coord: {
+    lng: { type: Number, required: true },
+    lat: { type: Number, required: true }
+  },
+  status: { type: String },
+  sog: { type: Number },
+  cog: { type: Number },
+  velocity: { type: Number },
+  mv: { type: String },
+  checksum: { type: String }
+}, {
+  timestamps: true,
+  collection: 'gprmc' })
+
+export default mongoose.model('Gprmc', GprmcSchema)
