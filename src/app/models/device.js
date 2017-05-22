34b060e57f23ae7eea5f3e266a4b6dc5067e6de5
Watchanon Numnam <jiramot@gmail.com>
2017-05-22 23:28:56 +0700

    Set unique to device name

--- src/app/models/device.js
+++ src/app/models/device.js
@@ -1,10 +1,12 @@
 'use strict'
 
-import mongoose, { Schema } from 'mongoose'
+import mongoose, {Schema} from 'mongoose'
 
 const DeviceSchema = new Schema({
-  name: { type: String, required: true },
-  vehical: { type: String, required: true }
-}, { collection: 'device' })
+  name: {type: String, required: true, unique: true},
+  vehical: {type: String, required: true}
+}, {
+  collection: 'device'
+})
 
 export default mongoose.model('Device', DeviceSchema)
---
+++ src/test/unit/models/device.test.js
@@ -0,0 +1,36 @@
+/* eslint-env mocha */
+
+'use strict'
+
+import mongoose from 'mongoose'
+import {Mockgoose} from 'mockgoose'
+const mockgoose = new Mockgoose(mongoose)
+
+import assert from 'assert'
+import config from '~/config/config'
+import Device from '~/app/models/device'
+
+mongoose.Promise = global.Promise
+
+describe('Gpgga Model', () => {
+  before(async() => {
+    await mockgoose.prepareStorage()
+    await mongoose.connect(config.db)
+  })
+
+  after(async() => {
+    await mongoose.connection.close()
+  })
+
+  afterEach(async() => {
+    await mockgoose.helper.reset()
+  })
+
+  it('should save gpgga success', async() => {
+    await new Device({name: 'device1', vehical: 'vihecal 1'}).save()
+
+    let count = await Device.count({})
+
+    assert.equal(count, 1)
+  })
+})
