1f49b0cac4b3c554526567d93322160e2e3ceb44
Watchanon Numnam <jiramot@gmail.com>
2017-05-22 23:29:55 +0700

    Add hash properties to location
    For checking duplicate

--- src/app/models/location.js
+++ src/app/models/location.js
@@ -1,16 +1,17 @@
 'use strict'
 
-import mongoose, { Schema } from 'mongoose'
+import mongoose, {Schema} from 'mongoose'
 
 const LocationSchema = new Schema({
-  type: { type: String, required: true },
-  date: { type: Date, required: true },
+  type: {type: String, required: true},
+  date: {type: Date, required: true},
   coord: {
-    lng: { type: Number, required: true },
-    lat: { type: Number, required: true }
+    lng: {type: Number, required: true},
+    lat: {type: Number, required: true}
   },
-  hdop: { type: Number },
-  vehical: { type: String }
-}, { collection: 'location' })
+  hdop: {type: Number},
+  vehical: {type: String},
+  hash: {type: String, required: true, unique: true}
+}, {collection: 'location'})
 
 export default mongoose.model('Location', LocationSchema)
--- src/test/unit/models/location.test.js
+++ src/test/unit/models/location.test.js
@@ -13,26 +13,32 @@ import Location from '~/app/models/location'
 mongoose.Promise = global.Promise
 
 describe('Location Model', () => {
-  before(async () => {
+  before(async() => {
     await mockgoose.prepareStorage()
     await mongoose.connect(config.db)
   })
 
-  after(async () => {
+  after(async() => {
     await mongoose.connection.close()
   })
 
-  afterEach(async () => {
+  afterEach(async() => {
     await mockgoose.helper.reset()
   })
 
-  it('should save location success', async () => {
-    let model = await new Location({ type: 'GPRMC',
-      date: new Date(),
-      coord: { lng: 100.92753163, lat: 13.099856641666667 },
-      vehical: '1',
-      hdop: null }
-    ).save()
+  it('should save location success', async() => {
+    let model = await new Location(createLocationWithKey('hash')).save()
     assert.equal(model.vehical, '1')
   })
 })
+
+let createLocationWithKey = (hash) => {
+  return {
+    type: 'GPRMC',
+    date: new Date(),
+    coord: {lng: 100.92753163, lat: 13.099856641666667},
+    vehical: '1',
+    hdop: null,
+    hash: hash
+  }
+}
